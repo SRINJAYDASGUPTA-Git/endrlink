5dfe6fba1ee7ff93efe4c39d86fa2c14069c62c3
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2025-06-24 01:14:58 +0530

    feat: Init with icon and favicon files

---
+++ backend/src/main/java/com/srinjaydg/endrlink/EndrlinkApplication.java
@@ -0,0 +1,13 @@
+package com.srinjaydg.endrlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndrlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndrlinkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/srinjaydg/endrlink/EndrlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinjaydg.endrlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndrlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
